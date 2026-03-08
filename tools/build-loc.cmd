88aadef643f86907ff46daa01cddc836fb4410ae
charliesh4rk <103455996+charliesh4rk@users.noreply.github.com>
2026-03-08 17:40:32 -0400

    Create build-loc.cmd
    b

---
+++ tools/build-loc.cmd
@@ -0,0 +1,5 @@
+@echo off
+cd strings
+node index.js build languages.loc --folder "C:\Users\Charle\Documents\GitHub\MinecraftConsoles\Minecraft.Client\Windows64Media\loc"
+cd ..
+echo Check folder for stuff
